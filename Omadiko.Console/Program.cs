abaec1eaec49068e58995990cc4532c9d2e394b9
Hector <hectorgatsos@gmail.com>
2021-06-25 18:25:35 +0300

    Add project files.

---
+++ Omadiko.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Omadiko.RepositoryServices;
+using Omadiko.Entities;
+
+namespace Omadiko.Console2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductRepository repo = new ProductRepository();
+
+            Product p = new Product() { Name = "Lakis", Price = 50000 };
+
+            repo.Insert(p);
+
+            foreach (var item in repo.GetAll())
+            {
+                Console.WriteLine(item.Name);
+            }
+          
+
+           
+        }
+
+
+
+    }
+
+}
